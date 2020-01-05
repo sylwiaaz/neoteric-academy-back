62c4473479d0cf236704b44ff2435d086cba11a7
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-06 00:14:58 +0100

    Change error auth message

--- src/exceptions/authTokenMissingException.ts
+++ src/exceptions/authTokenMissingException.ts
@@ -2,7 +2,7 @@ import HttpException from './httpException';
 
 class AuthTokenMissingException extends HttpException {
   constructor() {
-    super(401, 'Authentication token missing');
+    super(401, 'You are not authenticated to search remote job offers. Please, login or remove filters.');
   }
 }
 
