ae6568843ed6e98545fa9b2e649e916609d6c3ca
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-12 23:38:42 +0100

    Add check id is ObjectIf type, invalid id error

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import InvalidIdOfferException from '../exceptions/invalidIdOfferException';
 import NotFoundOfferException from '../exceptions/notFoundOfferException';
 import OffersNotFoundException from '../exceptions/offersNotFoundException';
 import Controller from '../interfaces/controller.interface';
@@ -31,17 +32,21 @@ class OffersController implements Controller {
 
   private getOfferById = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
     const id = request.params.id;
-    const offer = await offerModel.findById(id);
-    if (offer) {
-      response.send(offer);
+    if (id.match(/^[0-9a-fA-F]{24}$/)) {
+      const offer = await offerModel.findById(id);
+      if (offer) {
+        response.send(offer);
+      } else {
+        next(new NotFoundOfferException(id));
+      }
     } else {
-      next(new NotFoundOfferException(id));
+      next(new InvalidIdOfferException());
     }
   }
 
   private getPremiumOffer = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
     const id = request.params.id;
-    const offer = await offerModel.findOne({_id: id, premium: true});
+    const offer = await offerModel.findOne({ _id: id, premium: true });
     if (offer) {
       response.send(offer);
     } else {
@@ -49,46 +54,46 @@ class OffersController implements Controller {
     }
   }
 
-private getOffersByFilters = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+  private getOffersByFilters = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
 
-  let { place, tech, exp, minSal, maxSal}: any = request.params;
-  let remote: any = /(.*?)/;
-  if (place === 'all' || place === undefined) {
-    place = /(.*?)/;
-  } else if (place === 'remote') {
-    place = /(.*?)/;
-    remote = 'true';
-  } else {
-    place = this.capitalize(place);
-  }
+    let { place, tech, exp, minSal, maxSal }: any = request.params;
+    let remote: any = /(.*?)/;
+    if (place === 'all' || place === undefined) {
+      place = /(.*?)/;
+    } else if (place === 'remote') {
+      place = /(.*?)/;
+      remote = 'true';
+    } else {
+      place = this.capitalize(place);
+    }
 
-  tech = (tech === 'all' || tech === undefined) ? /(.*?)/ : tech.toLowerCase();
-  exp = (exp === 'all' || exp === undefined) ? /(.*?)/ : exp.toLowerCase();
-  minSal = minSal === undefined ? 0 : this.changeSalStringtoNumber(minSal);
-  maxSal = maxSal === undefined ? 51000 : this.changeSalStringtoNumber(maxSal);
+    tech = (tech === 'all' || tech === undefined) ? /(.*?)/ : tech.toLowerCase();
+    exp = (exp === 'all' || exp === undefined) ? /(.*?)/ : exp.toLowerCase();
+    minSal = minSal === undefined ? 0 : this.changeSalStringtoNumber(minSal);
+    maxSal = maxSal === undefined ? 51000 : this.changeSalStringtoNumber(maxSal);
 
-  const offers = await offerModel.find({
+    const offers = await offerModel.find({
       companyPlace: place,
       tech,
       experienceLevel: exp,
       remote,
-      minSal: {$lte: maxSal},
-      maxSal: {$gte: minSal},
-  });
+      minSal: { $lte: maxSal },
+      maxSal: { $gte: minSal },
+    });
 
-  if ( offers.length > 0) {
-    response.send(offers);
-  } else {
-    next(new OffersNotFoundException());
+    if (offers.length > 0) {
+      response.send(offers);
+    } else {
+      next(new OffersNotFoundException());
+    }
   }
-}
 
   private capitalize(s: string): any {
-    return s && s[0].toUpperCase() + s.slice(1);
+    return s[0].toUpperCase() + s.slice(1);
   }
 
   private changeSalStringtoNumber(s): number {
-      return s = +[s.slice(0, s.length - 1)] * 1000;
+    return s = +[s.slice(0, s.length - 1)] * 1000;
   }
 }
 
